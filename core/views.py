1fac34efc0331ed32ea72f821e76f4d91e24e258
Misael Centeno <misaeljohancehi@gmail.com>
2023-09-20 20:54:56 -0300

    beta 0.1

--- core/views.py
+++ core/views.py
@@ -4,28 +4,17 @@ from django.urls import reverse
 
 user1 = {
     "dni": 111111111,
-    "first_name": "Javier",
-    "last_name": "Milei",
-    "email": "javicho@gmail.com",
+    "first_name": "test",
+    "last_name": "test_last",
+    "email": "test@depotfy.com",
     "phone_number": 111111,
     "rol": "Warehouse Manager",
     "logged_in": False
 }
-user2 = {
-    "dni": 111111111,
-    "first_name": "Donald",
-    "last_name": "Trump",
-    "email": "el_peluca@gmail.com",
-    "phone_number": 111111,
-    "rol": "Invetory Manager",
-    "logged_in": False
-}
 
-users = [user1, user2]
+users = [user1]
 
 def logged_in(request):
-    # if request.POST['email'] in [user.get("email") for user in users]:
-        # user = list(filter(lambda u: (u["email"] == request.POST['email']), users))[0]
     if request.POST['email'] == user1.get("email"):
         user = user1
         user["logged_in"] = True
@@ -36,29 +25,33 @@ def logged_in(request):
 
 
 def index(request):
-    # if args == {}: return render(request, "core/login.html")
-    if not user1["logged_in"]: return render(request, "core/login.html") 
-    # context = user1
+    if not user1["logged_in"]: return render(request, "core/login.html")
     return render(request, "core/index.html", context={"user1":user1})
 
-# def ingresos(request):
-#     return HttpResponse("Ingresos")
+def ingresos(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/ingresos.html", context)
 
-# def pedidos(request):
-#     return HttpResponse("Pedidos")
+def pedidos(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/pedidos.html", context)
 
-# def stock(request):
-#     return HttpResponse("Stock")
+def stock(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/stock.html", context)
 
-# def inventarios(request):
-#     return HttpResponse("Inventarios")
+def inventarios(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/inventarios.html", context)
 
-# def posiciones(request):
-#     return HttpResponse("Posiciones")
+def posiciones(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/posiciones.html", context)
 
-# def usuarios(request):
-#     return HttpResponse("Usuarios")
+def usuarios(request):
+    context = {'nombre_usuario': 'Usuario de prueba'}
+    return render(request, "core/usuarios.html", context)
 
-# def usuario_detalle(request, usuario_id):
-#     return HttpResponse(f"Usuario Detalle {usuario_id}")
+def usuario_detalle(request, usuario_id):
+    return HttpResponse(f"Usuario Detalle {usuario_id}")
 
