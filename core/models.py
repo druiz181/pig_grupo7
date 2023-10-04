097160f44bc553f69a58f2b11e012b76711b561b
druiz181 <mistabs@gmail.com>
2023-10-04 16:49:56 -0300

    Primer modelo generado

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model): 
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    dni = models.IntegerField()
+    email = models.EmailField(blank=True, null=True)    
